5af50305883cfaf5b52ca9ef172106860c5e71e3
Burke Azbill <burkeazbill@users.noreply.github.com>
2026-01-07 17:47:14 -0500

    Add missing argument to New-ScheduledTaskTrigger

--- Tools/CreateTask.ps1
+++ Tools/CreateTask.ps1
@@ -1,6 +1,6 @@
 $action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "-File C:\hol\Tools\SetGuestinfoOvfEnv.ps1"
 # Create a trigger set to run 5 minutes from now.
-$trigger = New-ScheduledTaskTrigger -At (Get-Date).AddMinutes(2)
+$trigger = New-ScheduledTaskTrigger -At (Get-Date).AddMinutes(2) -Once
 
 # Set the principal to run as the administrator account since the script MUST run as administrator... because of the vmtoolsd command.
 $principal = New-ScheduledTaskPrincipal -UserId "Administrator" -LogonType ServiceAccount -RunLevel Highest 
